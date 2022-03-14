130d4ae492e2170047d861a079d22c5c6ad47040
GuoJiaqi66 <858064356@qq.com>
2022-03-14 12:11:23 +0800

    2022-03-06   清理代码 (bug: 一个人多次浏览，只想展示这个人最近一次的浏览-->未解决)

--- src/main/java/top/zwsave/zweapi/db/dao/PersonalMessageDao.java
+++ src/main/java/top/zwsave/zweapi/db/dao/PersonalMessageDao.java
@@ -28,30 +28,15 @@ public class PersonalMessageDao {
     }
 
     public HashMap selectAllNotReadPersonalMsg(Long userId) {
-        System.out.println("============= selectAllNotReadPersonalMsg" + userId + "==============");
         HashMap hashMap = new HashMap();
         List list = selectInit(userId, "article", "star");
-        /*if (list.size() == 0) {
-            list = Collections.singletonList("");
-        }*/
         hashMap.put("articleStar", list);
         List list1 = selectInit(userId, "article", "like");
-        /*if (list1.size() == 0) {
-            list1 = Collections.singletonList("");
-        }*/
         hashMap.put("articleLike", list1);
         List list2 = selectInit(userId, "video", "star");
-        /*if (list2.size() == 0) {
-            list2 = Collections.singletonList("");
-        }*/
         hashMap.put("videoStar", list2);
         List list3 = selectInit(userId, "video", "like");
-        /*if (list3.size() == 0) {
-            list3 = Collections.singletonList("");
-        }*/
         hashMap.put("videoLike", list3);
-
-        System.out.println("==========="+ hashMap + "============");
         // TODO: 2022-03-13 消息的回复,私信
         return hashMap;
     }
@@ -61,7 +46,6 @@ public class PersonalMessageDao {
         query.addCriteria(Criteria.where("targetUserId").is(userId+"").and("readFlag").is(false)
                 .and("type").is(type).and("clazz").is(clazz));
         List<PersonalMessage> articleStar = mongoTemplate.find(query, PersonalMessage.class);
-        System.out.println( "==========res========="+articleStar);
         return articleStar;
     }
 }
